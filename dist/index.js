c13586dcf0012d2a6274213dce87647c26c7e384
Bhavesh Solminde <bhaveshsolminde@gmail.com>
2025-12-26 20:58:35 +0530

    Initialize TypeScript project with basic structure and configuration

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+  return `Hello ${person}`;
+}
+console.log(greet("Bhavesh"));
+console.log(greet("Jayesh"));
+//# sourceMappingURL=index.js.map
